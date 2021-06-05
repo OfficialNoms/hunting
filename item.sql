04aff4f9cd6ccb58604940cbcf6776a27e204022
Linden <65407488+thelindat@users.noreply.github.com>
2021-06-06 00:20:58 +1000

    Fix the SQL query

--- item.sql
+++ item.sql
@@ -1,5 +1,5 @@
 -- Linden Inv + Legacy
 INSERT INTO `items` (`name`, `label`, `weight`, `can_remove`, `stackable`, `closeonuse`, `description`) VALUES
   ('meat','Meat', 450, 1, 0, 1, NULL),
-  ('leather', 'Leather', 450, 1, 0, 1, NULL),
+  ('leather', 'Leather', 450, 1, 0, 1, NULL)
 ;
